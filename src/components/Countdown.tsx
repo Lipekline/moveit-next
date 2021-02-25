be0ce32aaf264bfa9734dac95417d73f7b1a399f
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-02-25 12:00:12 -0300

    experience level

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,9 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css'
 
 let countdownTimeout:NodeJS.Timeout;
 
 export function Countdown(){
+
+    const { startNewChallenge } = useContext(ChallengesContext)
 
     const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
@@ -42,6 +45,7 @@ export function Countdown(){
 
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
